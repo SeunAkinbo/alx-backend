9cb810a4f70cba018014f870d6a0475553b59bc0
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-24 14:43:44 +0000

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 """Module - 4-mru_cache.py"""
 
+from base_caching import BaseCaching
 
+
+class MRUCache(BaseCaching):
+    """ MRUCache class that inherits from BaseCaching and implements
+        a MRU caching system
+    """
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.access_order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.access_order.remove(key)
+        self.cache_data[key] = item
+        self.access_order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            mru_key = self.access_order.pop()
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.access_order.remove(key)
+        self.access_order.append(key)
+        return self.cache_data[key]
