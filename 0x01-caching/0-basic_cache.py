3c2ac952bc617a3e0522aa5be5c8851ec090bc27
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-24 14:15:25 +0000

    Basic Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 """Module - 0-basic_cache.py"""
 
+from base_caching import BaseCaching
 
+
+class BasicCache(BaseCaching):
+    """ BasicCache class that inherits from BaseCaching and implements
+        a basic caching system without any limit
+    """
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
