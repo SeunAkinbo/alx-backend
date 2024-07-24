e1d99323e6cf256b180a8dea60ec2d471adf4736
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-24 14:35:18 +0000

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 """Module - 3-lru_cache.py"""
 
+from base_caching import BaseCaching
 
+
+class LRUCache(BaseCaching):
+    """ LRUCache class that inherits from BaseCaching and implements
+        an LRU caching system
+    """
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = self.order.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
