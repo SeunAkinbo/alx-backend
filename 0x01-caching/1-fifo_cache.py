9d5dfd70f2795ba74de82217ed9e8833fcfb592d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-24 14:23:38 +0000

    Fifo Caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
 """Module - 1-fifo_cache.py"""
 
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class that inherits from BaseCaching and implements
+        a FIFO caching system
+    """
+    def __init__(self):
+        """ Initialize the FIFOCache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = self.order.pop(0)
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
