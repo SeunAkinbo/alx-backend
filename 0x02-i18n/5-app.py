9ad6bc07485f4aad2905cc6d56dedb3598bb0529
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-31 00:11:01 +0100

    Mock Logging In

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -49,7 +49,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """The Index"""
-    return render_template('3-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
