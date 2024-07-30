693097c38fd95e954aa4f07c65d3c3ef13eee0f6
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-30 21:15:10 +0100

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Module - 0-app.py"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """The Index"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5005)
