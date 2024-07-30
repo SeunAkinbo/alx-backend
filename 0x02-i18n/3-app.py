337eacc847dc879e7969f1bb92db163de151dab7
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-30 23:06:42 +0100

    Parametrize Templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module - 0-app.py"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get the locale"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """The Index"""
+    title = _("home_title")
+    header = _("home_header")
+    return render_template('2-index.html', title=title, header=header)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5011)
